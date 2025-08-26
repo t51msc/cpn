bf5e4ce3d5c7f3c2c7aabb6196a1d9b56a5d853c
t51msc <t51msc@the-51.com>
2025-08-26 22:43:29 +0900

    Fix X Button in Mobile

--- src/components/common/CareerPathViewer.js
+++ src/components/common/CareerPathViewer.js
@@ -2731,7 +2731,7 @@ return (
                   className="absolute top-full mt-2 left-1/2 transform -translate-x-1/2 backdrop-blur-xl bg-black/95 rounded-xl border border-gray-700/50 shadow-2xl z-50 mobile-popup"
                   style={{
                     width: `${300 * scale}px`,  
-                    padding: `${18 * scale}px`,  
+                    padding: `${18 * scale}px ${24 * scale}px`,  
                     fontSize: `${11 * scale}px`
                   }}
                   onClick={(e) => e.stopPropagation()}>
@@ -2739,8 +2739,8 @@ return (
                     onClick={() => setMobilePopupNode(null)}
                     className="absolute text-gray-400 hover:text-white"
                     style={{
-                      top: `${10 * scale}px`,
-                      right: `${10 * scale}px`,
+                      top: `${14 * scale}px`,
+                      right: `${14 * scale}px`,
                       width: `${24 * scale}px`,
                       height: `${24 * scale}px`
                     }}>
